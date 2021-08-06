fc12297c343bf75d3462b6922901a47f0e1efa10
DESKTOP-DKSIK09\Admin <brzden32@gmail.com>
2021-08-06 14:39:37 -0300

    change url1

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/email', require('./email/email.controller'));
 // app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.PORT
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
