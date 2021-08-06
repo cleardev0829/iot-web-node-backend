c21ec54b85910758d467e3d6abec94461b8fd133
DESKTOP-DKSIK09\Admin <brzden32@gmail.com>
2021-08-06 17:13:28 -0300

    port chaging

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ app.use(errorHandler);
 
 // start server
 // const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
+
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
