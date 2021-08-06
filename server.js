8fba03fb6043971ebce8740bc6a6ae69c8f4e1d1
Win Nebergall <anatolloflint@gmail.com>
2021-08-07 05:09:27 +1000

    last commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/messages', require('./messages/messages.controller'));
 app.use('/email', require('./email/email.controller')); 
 
 // global error handler
-// app.use(errorHandler);
+app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
