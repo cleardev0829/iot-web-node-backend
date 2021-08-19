99ffbccb5253d936e5520af10a726a3a2d0e1ec2
Win Nebergall <anatolloflint@gmail.com>
2021-08-20 04:31:13 +1000

    no message

--- products/product.model.js
+++ products/product.model.js
@@ -1,20 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    uid: { type: String, unique: true, required: true },
-    name: { type: String, required: true },
-    address: { type: String, require: true },
-    categories: { type: Array },
-    createdDate: { type: Date, default: Date.now }
+  uid: { type: String, unique: true, required: true },
+  name: { type: String, required: true },
+  categories: { type: Array },
+  location: {
+    address: { type: String },
+    lat: { type: Number },
+    lng: { type: Number },
+  },
+  createdDate: { type: Date, default: Date.now },
 });
 
-schema.set('toJSON', {
-    virtuals: true,
-    versionKey: false,
-    transform: function (doc, ret) {
-        delete ret._id;
-    }
+schema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
 });
 
-module.exports = mongoose.model('lift', schema);
+module.exports = mongoose.model("lift", schema);
